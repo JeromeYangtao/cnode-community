53bb446fb77318fc4a2bd31cb0f6fd5b988f51fa
Thomson <779387850@qq.com>
2017-07-21 15:26:18 +0800

    增加user接口

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -1,12 +1,28 @@
 const http = require('http')
 const server = http.createServer()
 const port = 8000
+const url = require('url')
+
 
 // 假设一个用户，尝试增删改查
+
 let users = []
 
 // 当server接收到请求的时候触发
 server.on('request', (request, response) => {
+    // 解析后的URL
+    const parsedUrl = url.parse(request.url)
+
+    console.log('有人访问')
+
+    if (parsedUrl.path.indexOf('/user') === -1) {
+        response.statusCode = 403
+        response.end(`${response.statusCode} not allowed`)
+    }
+
+    //只增加/user接口
+
+
     // 根据请求方式不同做成响应
     switch (request.method) {
         case 'GET':
