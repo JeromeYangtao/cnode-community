fe0d8442fbe4961a0407901f5ea26bae57346ccd
Thomson <779387850@qq.com>
2017-07-21 14:54:48 +0800

    按照请求方法不同做出响应

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -1,5 +1,30 @@
 const http = require('http')
 const server = http.createServer()
+const port = 8000
+
+// 假设一个用户，尝试增删改查
+let users = []
+
+// 当server接收到请求的时候触发
+server.on('request', (request, response) => {
+    // 根据请求方式不同做成响应
+    switch (request.method) {
+        case 'GET':
+            break
+        case 'POST':
+            break
+        case 'PATCH':
+            break
+        case 'DELETE':
+            break
+    }
+})
 
 
-server.listen(8000)
+
+// 启动服务器，监听端口
+server.listen({
+    port: port,
+    host: 'localhost'
+})
+console.log(`正在监听${port}端口`)
