94cacdea2bf1dad3499a33116162760e6782bf25
Thomson <779387850@qq.com>
2017-07-23 18:57:18 +0800

    优化POST请求

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -5,13 +5,13 @@ const url = require('url')
 const querystring = require('querystring')
 
 // 假数据库，尝试增删改查
-let users =[]
+let users = []
 
 // 当server接收到请求的时候触发
 server.on('request', (request, response) => {
     // 解析后的URL
     // true可以让Json格式的parseUrl.query变成string格式
-    const parsedUrl = url.parse(request.url,true)
+    const parsedUrl = url.parse(request.url, true)
 
     // 判断是否为user接口
     if (parsedUrl.path.indexOf('/user') === -1) {
@@ -46,30 +46,23 @@ server.on('request', (request, response) => {
             }
             break
         case 'POST':
-            let user = '';
+            let data = '';
             // 监听data事件,接受到数据的第一个字节开始响应
             request.on('data', (buffer) => {
                 // http以buffer的形式传输数据
-                const userStr = buffer.toString();
-
-                // 确保user是Json格式
-                console.log(1)
-                let CT = request.headers['content-type'];
-                if (CT === 'application/json') {
-                    console.log(2)
-                    user = JSON.parse(userStr);
-                    users.push(user);
-                }
+                data += buffer.toString();
             })
 
             request.on('end', () => {
+                let user = JSON.parse(data);
+                users.push(user)
                 request.statusCode = 201;
                 response.end('Great! User created!');
             })
             break;
 
         case 'PATCH':
-            let username = parsedUrl.path.substring(6,parsedUrl.path.length)
+            let username = parsedUrl.path.substring(6, parsedUrl.path.length)
             // 监听data事件,接受到数据的第一个字节开始响应
             request.on('data', (buffer) => {
                 // http以buffer的形式传输数据
@@ -79,7 +72,7 @@ server.on('request', (request, response) => {
                 let CT = request.headers['content-type'];
                 if (CT === 'application/json') {
                     let update = JSON.parse(userStr);
-                    let user = users.find(u=>u.name===username)
+                    let user = users.find(u => u.name === username)
                     user.address = update.address
                 }
             })
@@ -93,14 +86,13 @@ server.on('request', (request, response) => {
             if (parsedUrl.path.indexOf('/user/') > -1) {
                 let username = parsedUrl.path.substring(6, parsedUrl.path.length)
                 let index = users.findIndex(u => u.name === username);
-                users.splice(index,1)
+                users.splice(index, 1)
                 response.statusCode = 200;
                 response.end(JSON.stringify(users));
             }
             break
     }
 })
-
 
 
 // 启动服务器，监听端口
