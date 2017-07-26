fc2885dfe3db2177acf03dcd056dc32d9c723a1a
Thomson <779387850@qq.com>
2017-07-26 15:26:29 +0800

    handlebars渲染动态网站，layout.html布局所有页面共享

--- express-2.1/app.js
+++ express-2.1/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var api = require('./routes/api');
+var hbs = require('hbs');
+var blogEngine = require('./blog');
+var bodyParser = require('body-parser');
 // var favicon = require('serve-favicon')
 
 // 设置express实例的参数
@@ -12,7 +15,9 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
 // 设定view engine变量，意为网页模板引擎
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
+
+app.engine('html', hbs.__express);
 
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(express.logger('dev'));
@@ -24,19 +29,23 @@ app.set('view engine', 'hbs');
 // 目录为demo/public/images，访问
 // 网址则显示为http://localhost:3000/images
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/api', api.index);
 
 app.get('/', function (req, res) {
-    res.sendfile('./views/index.html');
+    res.render('index', {title: "最近文章", entries: blogEngine.getBlogEntries()});
 });
 
 app.get('/about', function (req, res) {
-    res.sendfile('./views/about.html');
+    res.render('about', {title: "自我介绍", content: "自我介绍的内容"});
 });
 
-app.get('/article', function (req, res) {
-    res.sendfile('./views/article.html');
+app.get('/article/:id', function (req, res) {
+    var entry = blogEngine.getBlogEntry(req.params.id);
+    console.log(entry)
+    res.render('article', {blog: entry});
 });
 
 app.listen(app.get('port'));
