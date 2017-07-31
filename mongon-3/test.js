aef724c45fd8ba1f1a9d6c32a16b8256358b7aea
Thomson <779387850@qq.com>
2017-07-31 23:44:29 +0800

    mongoose实验

--- mongon-3/test.js
+++ mongon-3/test.js
@@ -1,24 +1,73 @@
-var MongoClient = require('mongodb').MongoClient;
-var DB_CONN_STR = 'mongodb://localhost:27017/runoob';
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost/test'
 
-var insertData = function (db, callback) {
-    //连接到表 site
-    var collection = db.collection('site');
-    //插入数据
-    var data = [{"name": "菜鸟教程", "url": "www.runoob.com"}, {"name": "菜鸟工具", "url": "c.runoob.com"}];
-    collection.insert(data, function (err, result) {
-        if (err) {
-            console.log('Error:' + err);
-            return;
-        }
-        callback(result);
-    });
+mongoose.connect(uri, {useMongoClient: true})
+const db = mongoose.connection
+const Schema = mongoose.Schema
+
+mongoose.Promise = global.Promise
+
+const UserSchema = new Schema({
+    name: {type: String, require: true, unique: true, enum: ['laoyang', 'laozhang', '']},
+    age: {type: Number, max: 90, min: [1, 'nobody is younger than 1 years old']}
+})
+
+UserSchema.methods.sayYourName = function () {
+    return this.name
 }
+UserSchema.statics.findByName = async function (name) {
+    return await this.findOne({name: name})
+}
+const UserModel = mongoose.model('user', UserSchema)
 
-MongoClient.connect(DB_CONN_STR, function (err, db) {
-    console.log("连接成功！");
-    insertData(db, function (result) {
-        console.log(result);
-        db.close();
-    });
+!(async () => {
+    let user = new UserModel({name: 'wwwww'})
+    await user.save()
+})()
+    .then(r => {
+    })
+    .catch(e => {
+    })
+
+
+// !(async () => {
+//     let found = await UserModel.findByName('laoyang')
+//     return found
+// })()
+//     .then(r => {
+//         console.log(r)
+//     })
+//     .catch(e => {
+//     })
+// !(async () => {
+//     let found = await UserModel.findOne({})
+//     console.log(found.sayYourName())
+// })()
+//     .then(r => {
+//     })
+//     .catch(e => {
+//     })
+
+// !(async () => {
+//     let created = await UserModel.create({
+//         name: 'laoyang',
+//         age: 89
+//     }).then()
+//     return created
+// })()
+//     .then(r => {
+//         console.log(r)
+//     })
+//     .catch(e => {
+//         console.log((e))
+//     })
+
+// db.close((err, result) => {
+//     console.log(err)
+//     console.log(result)
+// })
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
 });
