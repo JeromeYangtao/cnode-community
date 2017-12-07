ea0da947dac6a9b86f21a37fd20116ceb65fd87a
Thomson <779387850@qq.com>
2017-12-07 14:39:43 +0800

    update

---
+++ views/management/src/components/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="index">
+    <div>
+      <el-input v-model="phoneNumber" placeholder="手机号"></el-input>
+      <el-input v-model="password" placeholder="密码"></el-input>
+      <el-input v-model="name" placeholder="姓名"></el-input>
+      <el-input v-model="age" placeholder="年龄"></el-input>
+      <el-button type="primary" @click="addUser">新增用户</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+    data () {
+      return {
+        phoneNumber: '',
+        password: '',
+        name: '',
+        age: ''
+        // object: {
+        //   title: '新增用户',
+        //   inputs: []
+        // }
+      }
+    },
+    methods: {
+      addUser () {
+        console.log(1)
+      }
+    }
+  }
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
